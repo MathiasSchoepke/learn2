939c1507dda342520e508eb385294a7485ac78ea
Mathias Schoepke <mathias.schoepke@outlook.de>
2020-03-04 15:56:37 +0100

    fix @ViewScoped init() called twice

--- learn2/src/main/java/mathiasschoepke/ConfigurationProvider.java
+++ learn2/src/main/java/mathiasschoepke/ConfigurationProvider.java
@@ -5,27 +5,32 @@ import javax.servlet.ServletContext;
 import org.ocpsoft.rewrite.annotation.RewriteConfiguration;
 import org.ocpsoft.rewrite.config.Configuration;
 import org.ocpsoft.rewrite.config.ConfigurationBuilder;
+import org.ocpsoft.rewrite.servlet.config.Forward;
 import org.ocpsoft.rewrite.servlet.config.HttpConfigurationProvider;
-import org.ocpsoft.rewrite.servlet.config.rule.Join;
+import org.ocpsoft.rewrite.servlet.config.Path;
 
 @RewriteConfiguration
 public class ConfigurationProvider extends HttpConfigurationProvider {
 
 	@Override
 	public int priority() {
-		return 10;
+		return 0;
 	}
 
 	@Override
 	public Configuration getConfiguration(final ServletContext context) {
 		return ConfigurationBuilder.begin()
+				// .addRule().perform(Log.message(Level.INFO, "Rewrite is
+				// active."))
 
-				.addRule(Join.path("/").to("/index.xhtml"))
+				.addRule().when(Path.matches("/")).perform(Forward.to("/index.xhtml"))
 
-				.addRule(Join.path("/employees").to("/employees.xhtml"))
-				// .addRule(Join.path("/employees/add-employee").to("/employee.xhtml"))
-				.addRule(Join.path("/employees/{id}").to("/employee.xhtml"))
+				.addRule().when(Path.matches("/employees")).perform(Forward.to("/employees.xhtml"))
 
-				.addRule(Join.path("/projects").to("/projects.xhtml")).addRule(Join.path("/projects/{id}").to("/project.xhtml"));
+				.addRule().when(Path.matches("/employees/{id}")).perform(Forward.to("/employee.xhtml"))
+
+				.addRule().when(Path.matches("/projects")).perform(Forward.to("/projects.xhtml"))
+
+				.addRule().when(Path.matches("/projects/{id}")).perform(Forward.to("/project.xhtml"));
 	}
 }
