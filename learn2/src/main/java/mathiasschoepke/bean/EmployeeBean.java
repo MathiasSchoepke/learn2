58645bd113f8b71e84477a5c243b05ce2f91dce9
Mathias Schoepke <mathias.schoepke@outlook.de>
2020-03-04 15:58:53 +0100

    add update employee

--- learn2/src/main/java/mathiasschoepke/bean/EmployeeBean.java
+++ learn2/src/main/java/mathiasschoepke/bean/EmployeeBean.java
@@ -25,32 +25,63 @@ public class EmployeeBean implements Serializable {
 	@Getter
 	@Setter
 	private Employee emp;
+	@Getter
+	@Setter
+	private String name;
 
 	@Inject
 	SessionBean sb;
 
 	@PostConstruct
 	public void init() {
+		System.out.println("EmployeeBean Init()");
+		// Thread.dumpStack();
 		Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-		url = params.get("id");
-		String[] split = url.split("-");
-		if (split.length != 0) {
-			long id;
-			try {
-				id = (long) Long.parseLong(split[split.length - 1]);
-				emp = (Employee) readEmployee(id);
-			} catch (NumberFormatException nfe) {
-				nfe.printStackTrace();
-				emp = new Employee();
+		if (params.containsKey("id")) {
+			System.out.println(params.get("id"));
+			url = params.get("id");
+			String[] split = url.split("-");
+			if (split.length != 0) {
+				long id;
+				try {
+					id = (long) Long.parseLong(split[split.length - 1]);
+					emp = (Employee) readEmployee(id);
+					name = emp.getName();
+				} catch (NumberFormatException nfe) {
+					// nfe.printStackTrace();
+					System.out.println("no employee 1");
+				}
+			} else {
+				System.out.println("no employee 2");
 			}
-		} else {
-			emp = new Employee();
 		}
+		System.out.println(emp);
 	}
 
 	private Object readEmployee(long id) {
 		QEmployee employee = QEmployee.employee;
 		Employee emp = sb.getQueryFactory().selectFrom(employee).where(employee.id.eq(id)).fetchOne();
 		return emp;
+	}
+
+	public Object updateEmployee() {
+		System.out.println("updateEmployee");
+		emp.setName(name);
+		emp.setSeoUrl(convertNameToUrl(name));
+
+		System.out.println(emp);
+
+		sb.getEm().getTransaction().begin();
+		sb.getEm().merge(emp);
+		sb.getEm().getTransaction().commit();
+		return null;
+	}
+
+	private String convertNameToUrl(String name) {
+		String url = name.trim().toLowerCase().replaceAll("[^a-z0-9_\\s-]", "");
+		url = url.replaceAll("[\\s-]+", " ");
+		url = url.replaceAll("[\\s]", "-");
+		url += "-";
+		return url;
 	}
 }
