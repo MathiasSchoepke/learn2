9d2aca5016fec1f5c3d38ba133fdf1b0bd21fcac
Mathias Schoepke <mathias.schoepke@outlook.de>
2020-03-04 11:54:47 +0100

    use lombok's @EqualsAndHashCode annotation

--- learn2/src/main/java/mathiasschoepke/pojo/JobKey.java
+++ learn2/src/main/java/mathiasschoepke/pojo/JobKey.java
@@ -5,12 +5,14 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Embeddable
 @Getter
 @Setter
+@EqualsAndHashCode
 public class JobKey implements Serializable {
 
 	@Column(name = "employee_id")
@@ -25,36 +27,5 @@ public class JobKey implements Serializable {
 	public JobKey(Long employeeId, Long projectId) {
 		this.employeeId = employeeId;
 		this.projectId = projectId;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((employeeId == null) ? 0 : employeeId.hashCode());
-		result = prime * result + ((projectId == null) ? 0 : projectId.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		JobKey other = (JobKey) obj;
-		if (employeeId == null) {
-			if (other.employeeId != null)
-				return false;
-		} else if (!employeeId.equals(other.employeeId))
-			return false;
-		if (projectId == null) {
-			if (other.projectId != null)
-				return false;
-		} else if (!projectId.equals(other.projectId))
-			return false;
-		return true;
 	}
 }
--- learn2/src/main/java/mathiasschoepke/pojo/RatingKey.java
+++ learn2/src/main/java/mathiasschoepke/pojo/RatingKey.java
@@ -5,12 +5,14 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Embeddable
 @Getter
 @Setter
+@EqualsAndHashCode
 public class RatingKey implements Serializable {
 
 	@Column(name = "employee_id")
@@ -25,36 +27,5 @@ public class RatingKey implements Serializable {
 	public RatingKey(long employeeId, long skillId) {
 		this.employeeId = employeeId;
 		this.skillId = skillId;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((employeeId == null) ? 0 : employeeId.hashCode());
-		result = prime * result + ((skillId == null) ? 0 : skillId.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		RatingKey other = (RatingKey) obj;
-		if (employeeId == null) {
-			if (other.employeeId != null)
-				return false;
-		} else if (!employeeId.equals(other.employeeId))
-			return false;
-		if (skillId == null) {
-			if (other.skillId != null)
-				return false;
-		} else if (!skillId.equals(other.skillId))
-			return false;
-		return true;
 	}
 }
